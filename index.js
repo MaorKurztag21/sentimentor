e45292ef02a36c9b68fb2f9a70547730ddac9538
MaorKurztag21 <rnrwznr@gmail.com>
2021-09-20 17:58:36 +0300

    Fetch API link to the function

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 async function checker(){
-    let text=document.getElementsByTagName("textarea")[0].value;
+    let text=document.getElementsByTagName("textarea").value;
     let result=document.getElementById("result-place");
-result.innerHTML=`rate: <div id="Rate"></div>Text is: <div id="character"></div>`;
+    let source=await fetch("https://sentim-api.herokuapp.com/api/v1/",{
+    method: "POST",
+    headers: {
+        Accept: "application/json", 
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({"text": `${text}`}),
+    })
+    result.innerHTML=`rate: <div id="Rate"></div>Text is: <div id="character"></div>`;
 }
 
 let active=document.getElementsByTagName("button")[0];
