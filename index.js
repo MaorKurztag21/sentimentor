f541508b5bc12459f5faceb90b08e819c936bb12
MaorKurztag21 <rnrwznr@gmail.com>
2021-09-21 23:05:14 +0300

    Bonus+final css

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 async function checker(){
     let text=document.getElementsByTagName("textarea")[0].value;
     let result=document.getElementById("resultplace");
-    result.appendChild(document.createElement("img")).src='https://i.gifer.com/80ZN.gif';
+    let load=document.createElement("img");
+    load.id="load";
+    result.appendChild(load).src='https://i.gifer.com/80ZN.gif'
+    //result.appendChild(document.createElement("img")).src='https://i.gifer.com/80ZN.gif';
+    let cat=document.createElement("img");
+    // cat.classList.add="cat";
+    cat.id="cat"
+    cat.style.float ="left"
+
+    
     let source=await fetch("https://sentim-api.herokuapp.com/api/v1/",{
-    method: "POST",
-    headers: {
-        Accept: "application/json", 
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({"text": `${text}`}),
-})
-if(source.status>400){
-    result.innerHTML="";
-    result.textContent="It's an error";
-}
-else{
+        method: "POST",
+        headers: {
+            Accept: "application/json", 
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({"text": `${text}`}),
+    })
+    let statusNum=source.status;
+    if(statusNum>400){
+        result.textContent="It's an error";
+        cat.setAttribute("width", "200px");
+        cat.setAttribute("height", "200px");
+        result.appendChild(cat).src=`https://http.cat/${statusNum}` ; 
+    }
+    else{
 result.innerHTML=`Rate: <div id="rate"></div>Text emotion is: <div id="emotion"></div>`;
+cat.setAttribute("width", "200px");
+       cat.setAttribute("height", "200px");
+result.appendChild(cat).src=`https://http.cat/${statusNum}` ; 
 let solution=await source.json();
 let rate=document.getElementById("rate");
 let emotion=document.getElementById("emotion");
@@ -32,5 +47,5 @@ else if(solution.result.polarity<0){
 }
 
 }
-let active=document.getElementsByTagName("button")[0];
-active.addEventListener("click",checker);
+ let active=document.getElementsByTagName("button")[0];
+ active.addEventListener("click",checker);
